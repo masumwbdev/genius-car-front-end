d885f3ff2c39e34ddcf989c0bacec3e98beb04b9
masum691 <masumhu691@gmail.com>
2021-10-26 17:13:58 +0600

    added form

--- src/Pages/Home/AddService/AddService.js
+++ src/Pages/Home/AddService/AddService.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import './AddService.css'
 
 const AddService = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+    };
     return (
-        <div>
-            <h1>Add service</h1>
+        <div className="form-container">
+            <h2>Submit Form</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("name", { required: true, maxLength: 20 })} placeholder="Name" />
+                <textarea {...register("description")} placeholder="Description" />
+                <input type="number" {...register("price")} placeholder="Price" />
+                <input {...register("img")} placeholder="Image url" />
+                <input className="submit" type="submit" />
+            </form>
         </div>
     );
 };
