12caed4dfa8a94938a651dfad59c3b5150e4f918
masum691 <masumhu691@gmail.com>
2021-10-26 11:01:34 +0600

    changed config file

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
 console.log(process.env);
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
+    apiKey: "AIzaSyCDvIKp0oO2HJLSz4RxSQOnY5XXJSUuGS4",
+    authDomain: "machanic-frontend.firebaseapp.com",
+    projectId: "machanic-frontend",
+    storageBucket: "machanic-frontend.appspot.com",
+    messagingSenderId: "498967731829",
+    appId: "1:498967731829:web:7c8ee15a7ef1b88b40a23c"
+  };
 
-export default firebaseConfig;
+  export default firebaseConfig;
